20aa419c5a756f9c71131c40bb7d1648b2613f71
Anita Schurch <anita.schuerch@gmail.com>
2016-12-09 08:19:45 +0100

    reformatted the loop and added comments

--- determinereadlength.sh
+++ determinereadlength.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-for i in *fastq; do echo -n "$i "&& head -6 $i | tail -1 | wc -m; done
+#script to determine read lengths of fastq files
+for i in *fastq
+do 
+  echo -n "$i "&& head -6 $i | tail -1 | wc -m
+done
